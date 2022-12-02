9341f7b125ac886ccb4daea7bbf8301c40f90560
jonathan <97814319+jonathansfx@users.noreply.github.com>
2022-12-01 19:18:12 -0700

    fsgdg

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 // Caught Speeding CYU Asgn
+
+//HTML Variables
+let speedEl = document.getElementById("speed");
+let limitEl = document.getElementById("limit");
+let ticketEl = document.getElementById("ticket")
+
+//Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+//IF Statements
+if (limitEl - speedEl < 20) {
+ticketEl.innerHTML = ("SMALL TICKET"); 
+} else if (limitEl - speedEl > 20) {
+ticketEl.innerHTML = ("BIG TICKET");
+}
+
+
+}
