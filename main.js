143b7c5cb8d1a9a33abca2f136221e88a4e6fa4c
jonathansfx <jonathanluo22@gmail.com>
2022-12-05 14:45:45 -0700

    asdfjkbdlkjbndbhsdf

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // Caught Speeding CYU Asgn
 
 //HTML Variables
-let speedEl = document.getElementById("speed");
-let limitEl = document.getElementById("limit");
-let ticketEl = document.getElementById("ticket")
+let speedEl = document.getElementById("speed").value;
+let limitEl = document.getElementById("limit").value;
+let ticketEl = document.getElementById("ticket");
 
 //Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-//IF Statements
-if (limitEl - speedEl < 20) {
-ticketEl.innerHTML = ("SMALL TICKET"); 
-} else if (limitEl - speedEl > 20) {
-ticketEl.innerHTML = ("BIG TICKET");
-}
-
-
+  //IF Statements
+  if (speedEl - limitEl >= 20) {
+    ticketEl.innerHTML = "BIG TICKET";
+  } else if (speedEl - limitEl < 20) {
+    ticketEl.innerHTML = "SMALL TICKET";
+  } else if (speedEl <= limitEl) {
+    ticketEl.innerHTML = "NO TICKET";
+  }
 }
