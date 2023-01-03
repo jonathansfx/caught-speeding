1183ae5f21f8b671531528dd919a758631794fd8
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-01-03 16:59:57 -0700

    finish

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 // Caught Speeding CYU Asgn
 
+//Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
 //HTML Variables
 let speedEl = document.getElementById("speed").value;
 let limitEl = document.getElementById("limit").value;
 let ticketEl = document.getElementById("ticket");
 
-//Button Event Listener
-document.getElementById("btn").addEventListener("click", btnClicked);
+//IF Statements
+if (speedEl - limitEl >= 20) {
+ticketEl.innerHTML = "Big Ticket";
+} else if (speedEl - limitEl < 20 && speedEl - limitEl > 0) {
+  ticketEl.innerHTML = "Small Ticket";
+} else if (speedEl - limitEl == 0 || speedEl < limitEl) {
+ticketEl.innerHTML = "No ticket"
+}
 
-function btnClicked() {
-  //IF Statements
-  if (speedEl - 20 >= limitEl) {
-    ticketEl.innerHTML = "BIG TICKET";
-  } else if (speedEl - 20 <= limitEl) {
-    ticketEl.innerHTML = "SMALL TICKET";
-  } else if (limitEl <= speedEl) {
-    ticketEl.innerHTML = "NO TICKET";
-  }
 }
